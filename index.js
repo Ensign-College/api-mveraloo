d9271e5aea36ad4e75795f31303d710644d23a75
mveraloo <77229096+mveraloo@users.noreply.github.com>
2024-01-10 17:23:36 +0000

    Added express, dev container

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+// import is better than require. it is another way to import succesfully.
+// import express from 'express';
+// Express makes API's - connect fronend to database.
+const express = require('express');
+const app = express(); //create an express application
+app.listen(3000); //listen for web requests from the frondend and don't stop.
+
+const boxes = [
+    {boxId:1},
+    {boxId:2},
+    {boxId:3},
+    {boxId:4}
+];
+//1 parameter = URL
+//2 - a function to return boxes
+//req= the request from the browser
+//res= the response to the browser
+app.get('/boxes', (req,res)=>{
+    //send the boxes to the browser
+    res.send(JSON.stringify(boxes)); // convert boxes to a string
+}); //return boxes to the user
 console.log("Hello");
