ad555fa33b904cb9927dae812488a254c5cac8bf
mveraloo <mireyavera1511@gmail.com>
2024-02-05 21:04:58 -0700

    Connect the frontend to the Backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // import express from 'express';
 // Express makes API's - connect frontend to database.
 const express = require('express');
+const cors = require('cors');
 const Redis = require('redis');//import the redis library
 const bodyParser = require('body-parser'); //import the body parser
 //import express from 'express'
@@ -11,15 +12,18 @@ const redisClient = Redis.createClient({
 
 
 const app = express(); //create an express application
-const port = 3000; //port number
+const port = 3001; //port number
+
 
 app.use(bodyParser.json()); //user bodyparser
+app.use(cors()); //allow frontend to call backend 
+
 app.listen(port, ()=>{
     redisClient.connect(); //it connects to the database
     console.log(`API is Listening on port: ${port}`);
 }); //listen for web requests from the frondend and don't stop.
 
-
+//http://localhost:3000/boxes
 app.post('/boxes', async (req, res) => { //async means we will await promises
         const newBox = req.body;
         newBox.id = parseInt(await redisClient.json.arrLen('boxes', '$')) +1; //the user shouldn't choose the ID!
@@ -37,9 +41,8 @@ app.get('/boxes', async (req,res)=>{
     let boxes = await redisClient.json.get('boxes',{path: '$'}); //get the boxes 
     
     //send the boxes to the browser
-    res.send(JSON.stringify(boxes)); // convert boxes to a string
+    res.json(boxes[0]); // the boxes is an array of arrays, convert first element to a JSON string
 }); //return boxes to the user
-
 
 
 console.log("Hello");
